fc7cfa12dc341ab264f818a3a7a64d68023957b5
Rozemarin <mrozaeva0812@mail.ru>
2020-12-02 01:47:45 +0300

    some text

--- main.py
+++ main.py
@@ -1,8 +1,28 @@
-from PyQt5 import QtWidgets, uic
+from PyQt5 import QtWidgets
+from design import Ui_MainWindow  # импорт нашего сгенерированного файла
 import sys
 
-app = QtWidgets.QApplication([])
-win = uic.loadUi("design.ui")  # расположение вашего файла .ui
 
-win.show()
+class mywindow(QtWidgets.QMainWindow):
+    def __init__(self):
+        super(mywindow, self).__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        self.ui.pushButton.clicked.connect(self.btnClicked)
+        self.setWindowTitle('sevensolve')
+
+    def btnClicked(self):
+        if self.ui.text.isChecked():
+            pass
+        elif self.ui.image.isChecked():
+            pass
+        elif self.ui.sound.isChecked():
+            pass
+
+
+
+app = QtWidgets.QApplication([])
+application = mywindow()
+application.show()
+
 sys.exit(app.exec())
